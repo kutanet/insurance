7be6832af911657b336041e623ebff3e0ae701f6
Ruslan QA Engineer <73351847+Ruslan-Java-Automation@users.noreply.github.com>
2023-07-28 23:17:31 +0800

    new commit and push test July 28 23:07:37

--- src/test/java/definitions/PredefinedStepDefs.java
+++ src/test/java/definitions/PredefinedStepDefs.java
@@ -32,7 +32,7 @@ public class PredefinedStepDefs {
 
     @Then("I wait for {int} sec")
     public void iWaitForSec(int sec) throws Exception {
-        Thread.sleep(sec * 1000);
+        Thread.sleep(sec * 500);
     }
 
     @When("I maximize window")
